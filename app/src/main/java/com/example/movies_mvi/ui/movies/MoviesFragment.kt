a0ac8f504aad7a51109949ba3158be03b170ca84
Andjelkovic <uic34828@contiwan.com>
2020-03-20 10:54:29 +0100

    styling

--- app/src/main/java/com/example/movies_mvi/ui/movies/MoviesFragment.kt
+++ app/src/main/java/com/example/movies_mvi/ui/movies/MoviesFragment.kt
@@ -9,7 +9,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.movies_mvi.R
 import com.example.movies_mvi.model.Movie
